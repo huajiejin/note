0357c967794016c907b9adbaf38baafaea36187d
huajie <huajiejin@qq.com>
2019-03-29 17:14:54 +0800

    test circleci/config.yml

--- deploy.circleci.sh
+++ deploy.circleci.sh
@@ -2,7 +2,7 @@
 #! /bin/bash
 set -e
 
-vuepress build
+npx vuepress build
 
 NOW=`date "+%Y-%m-%d %H:%M:%S"`
 COMMIT_MSG="deploy $NOW"
@@ -14,11 +14,3 @@ git config user.email huajiejin@qq.com
 git add .
 git commit "-m$COMMIT_MSG"
 git push -f git@github.com:huajiejin/note.git master:gh-pages
-
-# cd -
-# git config user.name huajie
-# git config user.email huajiejin@qq.com
-# git add .
-# git commit "-m$COMMIT_MSG"
-# git pull
-# git push

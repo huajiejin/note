d749fd0868db68becc8bef62ca0081a1189c31a5
huajie <huajiejin@qq.com>
2019-04-12 15:47:20 +0800

    update util

--- .demo/dev/util.js
+++ .demo/dev/util.js
@@ -1,1 +1,12 @@
-console.log('hello, world!')
+const timeDiff = (earlier, later) => {
+  const DIFF_MS = (!earlier || !later) ? 0 : later - earlier
+  return {
+    day: Math.floor(DIFF_MS/1000/60/60/24),
+    hour: Math.floor(DIFF_MS/1000/60/60%24),
+    allHour: Math.floor(DIFF_MS/1000/60/60),
+    minute: Math.floor(DIFF_MS/1000/60%60),
+    second: Math.floor(DIFF_MS/1000%60),
+    millisecond: Math.floor(DIFF_MS%1000),
+  }
+}
+console.log(timeDiff(new Date(), new Date('2019-04-12 00:00:00')))

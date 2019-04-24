e514b714948f16cc8d4f595497e21d3428a3b4c9
huajie <huajiejin@qq.com>
2019-04-24 13:37:32 +0800

    update

---
+++ .vuepress/components/demo/random.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .random
+    .head
+      .item(v-for="(item, i) in list" :key="i" v-text="item")
+    .footer
+      button(@click="handleSelect") 选一个
+</template>
+
+<script>
+export default {
+  name: 'random',
+  data () {
+    return {
+      list: ['']
+    }
+  },
+  method: {
+    handleSelect () {
+      
+    }
+  }
+}
+</script>
